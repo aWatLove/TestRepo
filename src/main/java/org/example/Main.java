29dbc52a0dcaba35689da6449cc2240b329235f4
Vladislav <watislove4u@gmai.com>
2023-12-06 23:29:08 +0400

    some in dev 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,5 +11,6 @@ public class Main {
         System.out.println("some in dev");
         System.out.println("sdgdsg");
         System.out.println("sdgsdg");
+        System.out.println();
     }
 }
